bab3978ccdea0984c64fdfbff4fc81581e4f16ce
xannik <rasmus_lindy@hotmail.com>
2022-12-05 06:43:43 +0100

    part1 day3

---
+++ day03/src/main.rs
@@ -0,0 +1,166 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+/*
+
+    Lowercase item types a through z have priorities 1 through 26.
+    Uppercase item types A through Z have priorities 27 through 52.
+
+*/
+
+/*fn find_matching_item(
+    first_rucksack: &mut [u8],
+    second_rucksack: &mut [u8],
+    third_rucksack: &mut [u8],
+) -> u8 {
+    second_rucksack.sort_unstable();
+    first_rucksack.sort_unstable();
+
+    let mut matching_item: Vec<u8> = Vec::new();
+    let mut previous_item_f = 0;
+    let mut previous_item_s = 0;
+    for item_f in first_rucksack {
+        if previous_item_f != *item_f {
+            previous_item_f = *item_f
+        } else {
+            continue;
+        }
+        for item_s in second_rucksack {
+            if previous_item_s != *item_s {
+                previous_item_s = *item_s
+            } else {
+                continue;
+            }
+            if *item_f == *item_s {
+                matching_item.push(*item_f);
+            }
+        }
+    }
+    third_rucksack.sort_unstable();
+    for item_f in matching_item {
+        for item_s in third_rucksack {
+            if item_f == *item_s {
+                return item_f;
+            }
+        }
+    }
+    return u8::MAX;
+}*/
+
+/*fn get_solution_part2(path: &str) -> u32 {
+    let mut result = 0;
+    let mut rucksacks: Vec<Vec<u8>> = Vec::new();
+    if let Ok(lines) = read_lines(path) {
+        for line in lines {
+            rucksacks.push(line.unwrap().into_bytes());
+        }
+    }
+
+    for _i in 0..(rucksacks.len() / 3) {
+        let mut first_rucksack = rucksacks.pop().expect("error!");
+        let mut second_rucksack = rucksacks.pop().expect("error!");
+        let mut third_rucksack = rucksacks.pop().expect("error!");
+
+        first_rucksack.sort_unstable();
+        second_rucksack.sort_unstable();
+
+        let mut matching_item_list: Vec<u8> = Vec::new();
+        let mut matching_item;
+        let mut previous_item_f = 0;
+        let mut previous_item_s = 0;
+        for item_f in first_rucksack {
+            if previous_item_f != item_f {
+                previous_item_f = item_f
+            } else {
+                continue;
+            }
+            for item_s in second_rucksack {
+                if previous_item_s != item_s {
+                    previous_item_s = item_s
+                } else {
+                    continue;
+                }
+                if item_f == item_s {
+                    matching_item_list.push(item_f);
+                }
+            }
+        }
+        third_rucksack.sort_unstable();
+        for item_f in matching_item_list {
+            for item_s in third_rucksack {
+                if item_f == item_s {
+                    matching_item = item_f;
+                    break;
+                } else {
+                    matching_item = 255;
+                }
+            }
+        }
+        if matching_item == 255 {
+            continue;
+        }
+        if matching_item >= 65 && matching_item <= 96 {
+            result += (matching_item - 65 + 27) as u32;
+            println!("{}", matching_item - 65 + 27);
+        } else if matching_item >= 97 && matching_item <= 122 {
+            result += (matching_item - 97 + 1) as u32;
+            println!("{}", matching_item - 97 + 1);
+        }
+    }
+    return result;
+}*/
+
+fn get_solution_part1(path: &str) -> u32 {
+    let mut result = 0;
+    if let Ok(lines) = read_lines(path) {
+        for line in lines {
+            let mut first_compartment = line.unwrap().into_bytes();
+            let rucksack_length = first_compartment.len();
+            let mut second_compartment = first_compartment.split_off(rucksack_length / 2);
+            first_compartment.sort_unstable();
+            second_compartment.sort_unstable();
+            let mut matching_item = 0;
+            for item_f in &first_compartment {
+                for item_s in &second_compartment {
+                    if *item_f == *item_s {
+                        matching_item = *item_f;
+                        break;
+                    }
+                }
+            }
+            // a = 97, A = 65
+            if matching_item >= 65 && matching_item <= 96 {
+                result += (matching_item - 65 + 27) as u32;
+                println!("{}", matching_item - 65 + 27);
+            } else if matching_item >= 97 && matching_item <= 122 {
+                result += (matching_item - 97 + 1) as u32;
+                println!("{}", matching_item - 97 + 1);
+            }
+        }
+    }
+    return result;
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn main() {
+    let part = match env::var("part") {
+        Ok(val) => val,
+        Err(_e) => "part1".to_string(),
+    };
+
+    if part == "part2" {
+        //println!("{}", get_solution_part2("input.txt"));
+        println!("part 2");
+    } else {
+        println!("{}", get_solution_part1("input.txt"));
+    }
+}
